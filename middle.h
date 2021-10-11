16e10f63d9d2ce4e8e9748dd28666803e8d9d945
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-10-11 12:14:48 +0300

    Add files via upload

---
+++ middle.h
@@ -0,0 +1,29 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+#include <iostream>
+
+void itc_num_print(int num);
+int itc_len_num(long long num);
+int itc_sum_num(long long num);
+long long itc_multi_num(long long num);
+int itc_max_num(long long num);
+int itc_min_num(long long num);
+int itc_rev_num(long long num);
+int itc_null_count(long long num);
+bool itc_mirror_num(long long num);
+int itc_mirror_count(long long num);
+int itc_second_max_num(long long num);
+int itc_second_simple_max_num(long long num);
+long long itc_bin_num(long long num);
+long long itc_oct_num(long long num);
+long long itc_rev_bin_num(long long num);
+long long itc_rev_oct_num(long long num);
+
+long long itc_abs(long long num);
+long long itc_pow(long long num, int step);
+long long my_rev(long long num);
+long long my_ten_in_step(long long i);
+long long itc_covert_num(long long num, int i);
+long long itc_rev_covert_num(long long num, int i);
+
+#endif // MIDDLE_H_INCLUDED
