b5791ba6388d567db5b2ccb5df2ad0e3fd2ea355
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2021-10-15 10:03:48 +0300

    Add files via upload

--- rev_covert_num.cpp
+++ rev_covert_num.cpp
@@ -5,6 +5,8 @@ long long itc_rev_covert_num(long long num, int i)
     long long len = itc_len_num(num);
     if(num < 0)
         return -1;
+    if(i < 2 || i > 10)
+        return -1;
     if(itc_max_num(num) >= i)
         return -1;
 
